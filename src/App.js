28da4dd8306363f7e1a5d51a4375a6b233c48f10
jinglad <jihanchowdhury70@gmail.com>
2020-11-15 20:55:40 +0600

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './Components/Home/Home/Home';
+import Login from './Components/Login/Login/Login';
 
 function App() {
   return (
-    <div>
-      <h1>Hotel Finder Working</h1>
-      <h1>This is branch check</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
