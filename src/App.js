3e7a326a92aeab01896e2af90ce98451dff404b4
ayshi roy <ayshiroy22@gmail.com>
2020-11-17 01:06:25 -0800

    completed by oyshi

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home/Home";
-import Login from "./Components/Login/Login/Login";
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { createContext, useState } from "react";
 import HouseRentItemDetails from "./Components/Home/HouseRentItemDetails/HouseRentItemDetails";
+import Login from "./Components/Login/Login/Login";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
 
+export const UserContext = createContext();
 export const houseRentItemContext = createContext();
 function App() {
   const [houseRentItem, setHouseRentItem] = useState({});
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <houseRentItemContext.Provider value={[houseRentItem, setHouseRentItem]}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="/houseRentItemDetails">
-            <HouseRentItemDetails></HouseRentItemDetails>
-          </Route>
-        </Switch>
-      </Router>
-    </houseRentItemContext.Provider>
+    <UserContext.Provider value = {[loggedInUser, setLoggedInUser]}>
+      <houseRentItemContext.Provider value={[houseRentItem, setHouseRentItem]}>
+        <p>Name: {loggedInUser.name}</p>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <PrivateRoute path="/houseRentItemDetails">
+              <HouseRentItemDetails></HouseRentItemDetails>
+            </PrivateRoute>
+          </Switch>
+        </Router>
+      </houseRentItemContext.Provider>
+    </UserContext.Provider>
   );
 }
 
